bb1ea2da29de854cb18668e4efefb63975c5919a
Aakash Mukherjee <110044436+blackcode1996@users.noreply.github.com>
2023-02-24 05:17:12 +0530

    all_done

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const cookieParser = require('cookie-parser')
+const bodyParser=require("body-parser")
+const fileUpload=require("express-fileupload")
 const express = require('express')
 const app = express()
+
 
 const errorMiddleware=require("./middleware/error")
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(fileUpload())
 
 //Route Imports
 const product=require("./routes/productRoute")
