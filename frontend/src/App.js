4e8fa9d1202e60f8b792e832a8e785add09c5af6
shivam061095 <shivam061095@gmail.com>
2023-02-23 11:25:38 +0530

    hello

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
 import AllRoutes from './Router/AllRoutes';
-import Navbar from './Components/Navbar';
+
+
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      
       <AllRoutes />
     </div>
   );
